84ab84a6cf099f044f4a4b55a54858d2888a2e97
wenliang <liuwenliang001@163.com>
2024-06-22 21:55:24 +0800

    feat: jasypt

--- src/main/java/space/wenliang/ai/aigcplatformserver/AigcPlatformServerApplication.java
+++ src/main/java/space/wenliang/ai/aigcplatformserver/AigcPlatformServerApplication.java
@@ -10,7 +10,6 @@ import org.springframework.context.ApplicationContext;
 public class AigcPlatformServerApplication {
 
     public static void main(String[] args) {
-        System.setProperty("rm_model_dir", "/model");
         ApplicationContext context = SpringApplication.run(AigcPlatformServerApplication.class, args);
         System.out.println();
         System.out.println(STR."启动成功: http://127.0.0.1:\{context.getEnvironment().getProperty("server.port")}");
